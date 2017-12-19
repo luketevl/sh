6f8953aec592923622d6ad3006c433f588d50292
luketevl <luketevl@gmail.com>
2017-12-19 14:58:30 -0200

    new sh to pull

--- update_git_repos.sh
+++ update_git_repos.sh
@@ -3,23 +3,8 @@
 # store the current dir
 CUR_DIR=$1
 
-# Let the person running the script know what's going on.
-echo "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
+echo "Pulling in latest changes for all repositories...";
 
-# Find all git repositories and update it to the master latest revision
-for i in $(find . -name ".git" | cut -c 3-); do
-    echo "";
-    echo "\033[33m"+$i+"\033[0m";
+find $CUR_DIR -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \;
 
-    # We have to go to the .git parent directory to call the pull command
-    cd "$i";
-    cd ..;
-
-    # finally pull
-    git pull origin master;
-
-    # lets get back to the CUR_DIR
-    cd $CUR_DIR
-done
-
-echo "\n\033[32mComplete!\033[0m\n"
+echo "Complete";
