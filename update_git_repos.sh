3778bfd17c90899e1191e5b88be79972c30163e6
luketevl <luketevl@gmail.com>
2017-12-19 15:21:37 -0200

    update

--- update_git_repos.sh
+++ update_git_repos.sh
@@ -5,6 +5,6 @@ CUR_DIR=$1
 
 echo "Pulling in latest changes for all repositories...";
 
-find $CUR_DIR -type d -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull" \;
+find $CUR_DIR -type d -depth 1 -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull" \;
 
 echo "Complete";
